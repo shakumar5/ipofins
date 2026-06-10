c094c15f41dc6845479b5335e96501584914ac57
UtilityTools <apnadeshbharat01@gmail.com>
2026-06-10 21:36:06 +0530

    IPO FINS

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ export default defineConfig({
   ],
   output: 'static',
   build: {
-    inlineStylesheets: 'always',
+    inlineStylesheets: 'auto',
   },
   vite: {
     plugins: [tailwindcss()],
