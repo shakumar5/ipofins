24b4c567762d3c9d21039cb5efb2c58e732006a5
UtilityTools <apnadeshbharat01@gmail.com>
2026-06-11 07:28:26 +0530

    tools hub

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ export default defineConfig({
   ],
   output: 'static',
   build: {
-    inlineStylesheets: 'auto',
+    inlineStylesheets: 'always',
   },
   vite: {
     plugins: [tailwindcss()],
